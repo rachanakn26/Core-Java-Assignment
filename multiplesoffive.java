edf77243db0598281f94c65264e9c8ebf6595dbb
rachanakn26 <rachanakn26@gmail.com>
2025-12-05 14:07:51 +0530

    Add files via upload

---
+++ multiplesoffive.java
@@ -0,0 +1,24 @@
+package javaclass;
+
+import java.util.Scanner;
+
+public class multiplesoffive {
+	public static void printMultiples(int arr[]) {
+		int count=0;
+		for (int num:arr) {
+			if(num%5==0) {
+				count++;
+			}
+		}
+		System.out.println(count);
+}	
+public static void main(String[] args) {
+	Scanner sc=new Scanner (System.in);
+	int arr[]=new int[6];
+	System.out.println("Enter the array Elements");
+	for (int i = 0; i<arr.length; i++) {
+		arr[i]=sc.nextInt();
+	}
+	printMultiples(arr);
+}
+}
