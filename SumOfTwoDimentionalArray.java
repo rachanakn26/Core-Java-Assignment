6136de53b992b8164a9d3eae5b49e9e4e1b3a0c8
rachanakn26 <rachanakn26@gmail.com>
2025-12-08 20:45:09 +0530

    Add files via upload

---
+++ SumOfTwoDimentionalArray.java
@@ -0,0 +1,23 @@
+package javaclass;
+
+import java.util.Scanner;
+
+public class SumOfTwoDimentionalArray {
+public static void main(String[] args) {
+	Scanner sc= new Scanner(System.in);
+	int arr[][]=new int[2][2];
+	for (int i = 0; i < arr.length; i++) {
+		for (int j = 0; j < arr[i].length; j++) {
+			System.out.println("Enter the value for row" +(i+1)+"and column"+(j+1));
+			arr[i][j]=sc.nextInt();
+		}
+	}
+	int sum=0;
+	for (int i = 0; i < arr.length; i++) {
+		for (int j = 0; j < arr.length; j++) {
+			sum+=arr[i][j];
+		}
+}
+	System.out.println(sum);
+}
+}
